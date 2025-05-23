3ea792c204480b62e2803515ce715fedc9d073d6
Hptrivia <rasheed-ayodeji.owolabi@iubh.de>
2025-05-23 11:10:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ app = gr.mount_gradio_app(app, create_v2(), path="/v1")
 if __name__ == "__main__":
     uvicorn.run("main:app",
                 host="0.0.0.0",
-                port=int(os.environ.get("PORT", 8000)),
+                port=int(os.environ.get("PORT", 8080)),
                 log_level="info")
