e6c1611dfec8200df79ffe0120c81c936286f7bd
Hptrivia <rasheed-ayodeji.owolabi@iubh.de>
2025-06-12 19:15:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,4 +25,10 @@ def _():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 7860))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=port,
+        log_level="warning",    # only warning+ show
+        access_log=False        # disable the GET/200 lines
+    )
